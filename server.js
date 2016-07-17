3ac3182b1228e80c291d358782598a35878ba799
Gobelinus <silent.arora@gmail.com>
2016-07-17 11:24:49 +0000

    Render instructions jade template for routes other than timestamp

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ var express = require('express');
 var moment = require('moment');
 
 var app = express();
+app.set('view engine', 'jade');
 
 app.get('/:timeStr', function (req, res) {
+  // moment expects fload for unix timestamp for parsing, so parse input to float
+  // if valid number, use it as unix timestamp,
+  // if not, try to parse it as string.
   var timeStr = parseFloat(req.params.timeStr) * 1000;
   if (isNaN(timeStr)) {
     timeStr = req.params.timeStr;
@@ -20,13 +24,15 @@ app.get('/:timeStr', function (req, res) {
   res.send(ret_val);
 });
 
+// render instructions file by default
 app.use(function(req, res, next){
-  // convert this to send instructions
-  res.status(404);
-
   // respond with json
-  res.send({ error: 'Not found' });
+  // res.send({ error: 'Not found' });
 
+  var baseurl = req.protocol + '://' + req.get('host');
+  res.render('index', {
+    baseurl: baseurl
+  });
 });
 
 app.listen(8080, function () {
